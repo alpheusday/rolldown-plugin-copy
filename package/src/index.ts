f1c82ea387470b7df02ae1038c2d2fc69faeeaf1
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-05-05 05:22:40 +0800

    refactor: export missing types

--- package/src/index.ts
+++ package/src/index.ts
@@ -5,6 +5,10 @@ export default copy;
 export type { Options } from "#/@types/options";
 export type {
     Target,
+    TargetRename,
+    TargetRenameOptions,
+    TargetTransform,
+    TargetTransformOptions,
     WriteFileData,
 } from "#/@types/target";
 
